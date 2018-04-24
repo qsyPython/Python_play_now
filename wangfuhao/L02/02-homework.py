d99cf15c43a3f614db244583f7c4ad7500ec5bf5
wfh <shouwangwolf@163.com>
2018-04-24 14:47:05 +0800

    提交

--- wangfuhao/L02/02-homework.py
+++ wangfuhao/L02/02-homework.py
@@ -9,34 +9,9 @@ Python的第2期作业：
 """
 1.记录用户的账号  登录时间  登录的使用的系统
 """
-import time,platform
 
-class saveLogInfoTool():
-    def __init__(self,account_temp):
-        self.account = account_temp
-        self.nowTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-
-    def writeInfo(self,stop_time_temp):
-        try:
-            f = open("logInfo.txt","a+",encoding="utf-8")
-            sysInfo = platform.system()
-            info = ('账号:{0},开始时间{1},结束时间{2},使用系统:{3}\n'.format(self.account , self.nowTime, stop_time_temp , sysInfo))
-            f.writelines(info)
-        except Exception as e:
-            print(e)
-        finally:
-            f.close()
-
-def main():
-    account = input("请输入登录名:")
-    pwd = input("请输入密码:")
-    f = saveLogInfoTool(account)
-    while True:
-        logout = input('请使用该系统.....(按e退出):')
-        if logout == "e":
-            stopTime = time.strftime("%Y-%m-%d %H:%M:%S", time.localtime())
-            f.writeInfo(stopTime)
-            break
+from saveLogInfoTool import *
 
 if __name__ == '__main__':
-    main()
+    log_info = saveLogInfoTool()
+    log_info.main()
