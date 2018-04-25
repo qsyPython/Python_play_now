c26a8b6f46517ed3be14e7dc44a75c8acfaf3945
吴林丰 <hanshanshi@7lk.com>
2018-04-25 23:40:44 +0800

    作业2

---
+++ wulinfeng/L2/WLFZYTwo/WLFMainWord.py
@@ -0,0 +1,12 @@
+# -*- coding:utf-8 -*-
+
+from PWwlfCong import WLFMethodTools
+
+class WLFMainWord:
+    def prf(self):
+        tools = WLFMethodTools.WLFMethodTools
+        tools.doLoginAera(tools)
+
+
+mainPro = WLFMainWord()
+mainPro.prf()
