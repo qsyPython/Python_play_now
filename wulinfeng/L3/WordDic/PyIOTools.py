d1a657e55bd5088ca453f6b84dc5d9d01770d191
吴林丰 <hanshanshi@7lk.com>
2018-05-17 16:31:15 +0800

    修改

--- wulinfeng/L3/WordDic/PyIOTools.py
+++ wulinfeng/L3/WordDic/PyIOTools.py
@@ -1,18 +1,31 @@
-from pylab import *
+# from pylab import *
+# import matplotlib.pyplot as plt
+# import matplotlib.ticker as ticker
+
 import matplotlib.pyplot as plt
-import matplotlib.ticker as ticker
+import numpy as np
+import matplotlib as mpl
+
 class PyIOToolClass(object):
     def drawChat(self,xlist = [],ylist = []):
-        mpl.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
-        mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+        # mpl.rcParams['font.sans-serif'] = ['SimHei']  # 指定默认字体
+        # mpl.rcParams['axes.unicode_minus'] = False  # 解决保存图像是负号'-'显示为方块的问题
+        # plt.bar(range(len(xlist)), xlist, color='rgb', tick_label=ylist)
+        # plt.xticks(range(len(ylist)),rotation=90)
+        # # plt.show()
+        # plt.xticks(fontsize=10)
+        # plt.tight_layout() #显示全
+        # plt.savefig("examples.pdf")
+        # plt.close()
+
+        mpl.rcParams['font.sans-serif'] = ['FangSong']
+        mpl.rcParams['axes.unicode_minus'] = False
         plt.bar(range(len(xlist)), xlist, color='rgb', tick_label=ylist)
-        plt.xticks(range(len(ylist)),rotation=90)
-        # plt.show()
-        plt.figaspect(10)
-        plt.figure(figsize=(10,5))
-        plt.xticks()
+        # plt.bar(ylist,xlist)
+        plt.xticks(range(len(ylist)), rotation=90,fontsize=7)
+        # plt.xticks(fontsize=7)
+        plt.yticks(fontsize=15)
+        plt.title("全国空气质量柱状图",fontsize = 15)
         plt.tight_layout() #显示全
         plt.savefig("examples.pdf")
         plt.close()
-
-
