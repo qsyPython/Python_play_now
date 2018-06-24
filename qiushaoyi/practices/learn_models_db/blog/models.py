3be0d2f982ac7881467146af8b5417b44ae6ea86
qsyMac <qsy118614@163.com>
2018-06-25 00:23:13 +0800

    提交models

--- qiushaoyi/practices/learn_models_db/blog/models.py
+++ qiushaoyi/practices/learn_models_db/blog/models.py
@@ -1,6 +1,7 @@
 
 #========= # QuerySet基础使用 =========
 # from django.db import models
+
 # class Blog(models.Model):
 #     name = models.CharField(max_length=100)
 #     tagline = models.TextField()
@@ -38,38 +39,66 @@
 
 #========= # QuerySet进阶使用 # =========
 
-from __future__ import unicode_literals
+# from __future__ import unicode_literals
+# from django.db import models
+# from django.utils.encoding import python_2_unicode_compatible
+# from django.contrib import admin
+#
+# @python_2_unicode_compatible
+# class Author(models.Model):
+#     name = models.CharField(max_length=50)
+#     qq = models.CharField(max_length=10)
+#     addr = models.TextField()
+#     email = models.EmailField()
+#
+#     def __str__(self):
+#         return self.name
+#
+# @python_2_unicode_compatible
+# class Article(models.Model):
+#     title = models.CharField(max_length=50)
+#     author = models.ForeignKey(Author,on_delete=models.CASCADE)
+#     content = models.TextField()
+#     score = models.IntegerField()
+#     tags = models.ManyToManyField('Tag') #会单门建table
+#
+#     def __str__(self):
+#         return self.title
+#
+# @python_2_unicode_compatible
+# class Tag(models.Model):
+#     name = models.CharField(max_length=50)
+#
+#     def __str__(self):
+#         return self.name
+
+# =========# Django 自定义Field #=========
+
 from django.db import models
-from django.utils.encoding import python_2_unicode_compatible
-from django.contrib import admin
 
-@python_2_unicode_compatible
-class Author(models.Model):
-    name = models.CharField(max_length=50)
-    qq = models.CharField(max_length=10)
-    addr = models.TextField()
-    email = models.EmailField()
+class CompressedTextField(models.Model):
+    def from_db_value(self, value, expression, connection, context):
+        if not value:
+            return value
+        try:
+            return value.decode('base64').decode('bz2').decode('utf-8')
+        except Exception:
+            return value
 
-    def __str__(self):
-        return self.name
+    def to_python(self,value):
+        if not value:
+            return value
+        try:
+            value.decode('base64')
+            return value
+        except Exception:
+            try:
+                return value.encode('utf-8').encode('bz2').encode('base64')
+            except Exception:
+                return value
 
-@python_2_unicode_compatible
-class Article(models.Model):
-    title = models.CharField(max_length=50)
-    author = models.ForeignKey(Author,on_delete=models.CASCADE)
-    content = models.TextField()
-    score = models.IntegerField()
-    tags = models.ManyToManyField('Tag') #会单门建table
 
-    def __str__(self):
-        return self.title
 
-@python_2_unicode_compatible
-class Tag(models.Model):
-    name = models.CharField(max_length=50)
-
-    def __str__(self):
-        return self.name
 
 
 
--- qiushaoyi/practices/learn_models_db/people/models.py
+++ qiushaoyi/practices/learn_models_db/people/models.py
@@ -8,6 +8,7 @@ class Worker(models.Model):
     def __str__(self):
         return self.name + str(self.age)
 
+
 # 创建了student的表
 class Student(models.Model):
     name = models.CharField(max_length=30)
